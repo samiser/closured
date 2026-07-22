01edb5b48e1ade1f61e0edccb2d60988a4501571
Samiser <github@me.samiser.xyz>
2026-07-22 15:58:27 +0000

    flake update

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    fenix.url = "github:nix-community/fenix";
     crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = {
     self,
